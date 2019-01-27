4bb7e880abcd8d1f3d3b6e6ddf2e4119338ba009
Anderson Melo <anderson.90@gmail.com>
2019-01-26 22:27:19 -0200

    update final no app sorteio

--- PrimeiroProjeto/app/src/main/java/me/andersonmelo/primeiroprojeto/MainActivity.java
+++ PrimeiroProjeto/app/src/main/java/me/andersonmelo/primeiroprojeto/MainActivity.java
@@ -2,6 +2,10 @@ package me.andersonmelo.primeiroprojeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView textoResultado = findViewById(R.id.textoResultado);
+
+        int numero = new Random().nextInt(11);
+
+        textoResultado.setText("O número selecionado é: " + numero);
     }
 }
