65d148eb35e74775a779b5cc93cf6c989f864cfc
Anderson Melo <anderson.90@gmail.com>
2019-02-05 00:26:45 -0200

    seção 09 -> 98%

---
+++ Componentesdeinterface/Seekbar/app/src/main/java/me/andersonmelo/seekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package me.andersonmelo.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SeekBar seekBar;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        textResultado = findViewById(R.id.textResultado);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textResultado.setText("Progresso: " + seekBar.getProgress() + " / " + seekBar.getMax() );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"Seekbar alterado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Seekbar nao está pressionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
