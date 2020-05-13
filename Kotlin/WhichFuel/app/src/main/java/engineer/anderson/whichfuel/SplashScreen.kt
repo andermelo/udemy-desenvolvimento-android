1ca015c5fc854854f085f9316e18a6b6f98ecd0e
andermelo <anderson.90@gmail.com>
2020-05-13 14:28:48 -0300

    update splashscreen

--- Kotlin/WhichFuel/app/src/main/java/engineer/anderson/whichfuel/SplashScreen.kt
+++ Kotlin/WhichFuel/app/src/main/java/engineer/anderson/whichfuel/SplashScreen.kt
@@ -12,14 +12,10 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-
         Handler().postDelayed({
-            // This method will be executed once the timer is over
-            // Start your app main activity
 
             startActivity(Intent(this,MainActivity::class.java))
 
-            // close this activity
             finish()
         }, SPLASH_TIME_OUT)
     }
